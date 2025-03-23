beae65296e41b026cc94f0a02170ffdeab96ecfb
Chu0019 <Don1994109>
2025-03-23 17:15:54 +0800

    Fix: Bind to 0.0.0.0 and use PORT for Render

--- app.py
+++ app.py
@@ -288,5 +288,8 @@ def list_table():
     '''
     return render_template_string(table_html, refills=pending_refills, generate_barcode=generate_barcode)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
